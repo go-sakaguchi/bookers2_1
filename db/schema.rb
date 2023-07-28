53c17c30510e3a3c0abb616e27e4b3f901b45797
go-sakaguchi <123789515+go-sakaguchi@users.noreply.github.com>
2023-07-28 15:41:56 +0000

    install devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_26_100915) do
+ActiveRecord::Schema.define(version: 2023_07_28_141839) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.integer "user"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
